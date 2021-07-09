11033381a29c077fa8311d123936423629c52c64
Guangzhao Wu <wug10@medtronic.com>
2021-07-09 14:57:21 -0400

    test01

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Manually Reviewed DevOps World!'));
+app.get('/', (req, res) => res.send('Hello Test01!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
